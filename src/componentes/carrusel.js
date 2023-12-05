bf2959fa16ef2b148085a8b29ec0abec07442c52
Ignacio Zamo <ignaciozamo1@gmail.com>
2023-12-05 19:18:42 -0300

    Se agrega apartado de About

---
+++ src/componentes/carrusel.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const carouselInner = document.querySelector('.carousel-inner');
+    const items = document.querySelectorAll('.carousel-item');
+    const totalItems = items.length;
+    let currentIndex = 0;
+
+    setInterval(() => {
+      currentIndex = (currentIndex + 1) % totalItems;
+      updateCarousel();
+    }, 4500); // Cambia la imagen cada 4 segundos
+
+    function updateCarousel() {
+      const newTransformValue = -currentIndex * 100 + '%';
+      carouselInner.style.transform = 'translateX(' + newTransformValue + ')';
+    }
+  });
